0224edaff1157d7643a48ce0848b49cb53eb9439
boldPro <probold777@gmail.com>
2024-03-18 11:51:29 -0400

    footer and header color styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,11 @@ export default function Footer() {
     return (
       <div className="footer">
         {/* <hr/> */}
-        <a target="_blank" href="https://www.google.com/maps/dir//1102+Salem+Ave,+Dayton,+OH+45406/@39.7746572,-84.2196603,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x88408122b505a61d:0xc35c5f962acf2cf5!2m2!1d-84.2174716!2d39.7746572"> <i className="fa fa-map-marker" aria-hidden="true"></i>&nbsp;1102 Salem Ave. Dayton, OH 45406</a>
+        <a target="_blank" href="https://www.google.com/maps/dir//GREEN+FOREST+APARTMENTS,+300-320+Forest+Park+Dr,+Dayton,+OH+45405/@39.8093879,-84.2187984,15z/data=!4m17!1m7!3m6!1s0x884081b53ea880a5:0xcc7c52c501f344ab!2sGREEN+FOREST+APARTMENTS!8m2!3d39.8091463!4d-84.2185168!16s%2Fg%2F11gr426pfx!4m8!1m0!1m5!1m1!1s0x884081b53ea880a5:0xcc7c52c501f344ab!2m2!1d-84.2185168!2d39.8091463!3e3?entry=ttu"> <i className="fa fa-map-marker" aria-hidden="true"></i>&nbsp;300-320 Forest Park Dr, Dayton, OH 45405</a>
         <br/>
-        <a className="social" href="https://www.facebook.com/SalemCrownApts/" target="_blank"><i className="fa fa-facebook-square" aria-hidden="true"></i></a>
+        <a className="social" href="https://www.facebook.com/GreenForestApts/" target="_blank"><i className="fa fa-facebook-square" aria-hidden="true"></i></a>
         &nbsp;  &nbsp;
-        <a className="social" target="_blank" href="https://www.yelp.com/biz/salem-crown-apartments-dayton"><i className="fa fa-yelp" aria-hidden="true"></i></a>
+        <a className="social" target="_blank" href="https://www.yelp.com/biz/green-forest-apartments-dayton"><i className="fa fa-yelp" aria-hidden="true"></i></a>
         <br/>
       <span className= "copy"> &copy;  Copyright 2024 Green Forest Apartments. </span>
       {/* &nbsp; &nbsp; &nbsp; */}
