ab45831c058cc807bd987f2e17073c4511988a04
boldPro <probold777@gmail.com>
2024-03-18 11:08:33 -0400

    added images to galery and carousel

--- client/src/components/AboutUsinfo.js
+++ client/src/components/AboutUsinfo.js
@@ -4,6 +4,25 @@ import { useState } from "react";
 
 import FsLightbox from "fslightbox-react";
 
+import newKitchenImg from "../assets/apatrment/kitchen_new2.jpg"
+import newKitchenImg2 from "../assets/apatrment/kitchen_new.jpg"
+import oldCloset from "../assets/apatrment/8.jpg"
+import oldLiving from "../assets/apatrment/living_old.jpg"
+import oldLiving2 from "../assets/apatrment/living_entry_old.jpg"
+import newLiving2 from "../assets/apatrment/living_new.jpg"
+import newLiving3 from "../assets/apatrment/Living_new3.jpg"
+import oldBathroom from "../assets/apatrment/Bathroom_old.jpg"
+import newBedroom from "../assets/apatrment/Bedroom_new.jpg"
+
+// import oldBedroom from "../assets/apatrment/bedroom_old.jpg"
+import floorPlan from "../assets/apatrment/floor_plan.png"
+import oldBedroom2 from "../assets/apatrment/bedroom_old2.jpg"
+import oldKitchen from "../assets/apatrment/kitchen_old.jpg"
+import appliances from "../assets/apatrment/appliences.jpg"
+
+import outside from "../assets/outside.jpg"
+import outsideAir from "../assets/outside above.jpg"
+
 export default function AboutUsinfo() {
       const [slide, setSlide] = useState(false);
   return (
@@ -26,27 +45,27 @@ export default function AboutUsinfo() {
             className="about infobox"
           >
             <p>
-              Salem Crown Apartments are fully remodeled with newly painted
+              Green Forest Apartments are fully remodeled with newly painted
               walls, remodeled bathroom and kitchen, luxury vinyl plank floors,
               and newer appliances that consist of a stove, fridge and
               dishwasher.{" "}
             </p>
-            <p>
+            {/* <p>
               The features inside this apartment are above the rest! Individual
               Furnace Units, new windows and doors, 3 closets, laundry area,
               secure entrances.{" "}
-            </p>
+            </p> */}
             <p>
-              Conveniently located 2 miles downtown Dayton, and close to Kroger
-              (2 miles) and Dayton Police Department (2 miles).
+              Conveniently located 3 miles downtown Dayton, and close to Kroger
+              (2 miles).
             </p>
             <p>
               <strong>RTA stop right on the corner</strong>{" "}
             </p>
             <p>
-              We strive to provide nice, clean, comfortable, and affordable
+              We strive to provide nice, clean, fully remodeled, comfortable, and affordable
               housing for responsible residents! 27-unit apartment complex under
-              new management (fully remodeled in 2018-2019)
+              new management
             </p>
           </div>
         </div>
@@ -177,33 +196,6 @@ No pets over 25 lbs.
         </div>
 
 
-        {/* <div className="box-container">
-          <h1 data-aos="zoom-in" data-aos-duration="2000" className="title">
-            Video Tour
-          </h1>
-          {/* <iframe
-            title="virtual tour about"
-            data-aos="zoom-in"
-            data-aos-delay="300"
-            data-aos-duration="1500"
-            className="virtual-tour"
-
-            src="https://www.google.com/maps/embed?pb=!4v1710606854736!6m8!1m7!1sCAoSLEFGMVFpcE5ydkxPVFJCNkdEN29yd09PdVM5R0RYWUhSWWRNQ2ZiWFFIZ0VY!2m2!1d39.8092414!2d-84.218333!3f137.19804953607814!4f-1.5282579253473756!5f0.7820865974627469"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>          
-
-        <iframe data-aos="zoom-in"
-            data-aos-delay="300"
-            data-aos-duration="1500" className="virtual-tour video" src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FGreenForestApts%2Fvideos%2F537745906753895%2F&show_text=false&width=560&t=2" width="560" height="314" style={{border:"none", overflow: "hidden"}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
-        </div> */}
-
-
-
-
-
-
         <div className="box-container">
           <div className="container">
             <h1 data-aos="zoom-in" data-aos-duration="100" className="title">
@@ -217,7 +209,7 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--1"
               >
                 <img
-                  src="https://i.imgur.com/ILzD0HZ.jpg"
+                  src={newKitchenImg}
                   className="gallery__img"
                   alt="Kitchen"
                 />
@@ -230,9 +222,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--2"
               >
                 <img
-                  src="https://i.imgur.com/R16jAox.jpg"
+                  src={newKitchenImg2}
                   className="gallery__img"
-                  alt="Bathroom"
+                  alt="Kitchen"
                 />
               </figure>
 
@@ -243,9 +235,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--3"
               >
                 <img
-                  src="https://i.imgur.com/t5fnmv0.jpg"
+                  src={oldLiving2}
                   className="gallery__img"
-                  alt="Bedroom"
+                  alt="Living Room"
                 />
               </figure>
 
@@ -256,9 +248,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--4"
               >
                 <img
-                  src="https://i.imgur.com/J4xLkW4.jpg"
+                  src={newLiving2}
                   className="gallery__img"
-                  alt="Bedroom"
+                  alt="Living Room"
                 />
               </figure>
 
@@ -269,9 +261,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--5"
               >
                 <img
-                  src="https://i.imgur.com/9Pc0exQ.jpg"
+                  src={newLiving3}
                   className="gallery__img"
-                  alt="Bedroom2"
+                  alt="Living Room"
                 />
               </figure>
 
@@ -282,9 +274,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--6"
               >
                 <img
-                  src="https://i.imgur.com/YUamIMj.jpg"
+                  src={oldLiving}
                   className="gallery__img"
-                  alt="Dining"
+                  alt="Living Room"
                 />
               </figure>
 
@@ -295,9 +287,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--7"
               >
                 <img
-                  src="https://i.imgur.com/oYV2yrP.jpg"
+                  src={oldBathroom}
                   className="gallery__img"
-                  alt="Kitchen Bar"
+                  alt="Bathroom"
                 />
               </figure>
 
@@ -308,9 +300,9 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--8"
               >
                 <img
-                  src="https://i.imgur.com/yafqqml.jpg"
+                  src={newBedroom}
                   className="gallery__img"
-                  alt="Living Room Patio Enterance"
+                  alt="Bedroom"
                 />
               </figure>
 
@@ -321,13 +313,13 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--9"
               >
                 <img
-                  src="https://i.imgur.com/jB7Yruf.jpg"
+                  src={oldCloset}
                   className="gallery__img"
-                  alt="Hallway"
+                  alt="Closet"
                 />
               </figure>
 
-              <figure
+              {/* <figure
                 data-aos="zoom-in-up"
                 data-aos-delay="1800"
                 data-aos-duration="1000"
@@ -338,7 +330,7 @@ No pets over 25 lbs.
                   className="gallery__img"
                   alt="Living Room Vaulted Ceilings"
                 />
-              </figure>
+              </figure> */}
 
               <figure
                 data-aos="fade-up-right"
@@ -347,7 +339,7 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--11"
               >
                 <img
-                  src="https://i.imgur.com/U1Mtm2b.jpg"
+                  src={outside}
                   className="gallery__img"
                   alt="Outside"
                 />
@@ -360,26 +352,26 @@ No pets over 25 lbs.
                 className="gallery__item gallery__item--12"
               >
                 <img
-                  src="https://i.imgur.com/CwSBvsh.jpg"
+                  src={floorPlan}
                   className="gallery__img"
                   alt="Floor Plan"
                 />
               </figure>
 
               <figure
-                data-aos="fade-up-left"
+                data-aos="fade-up"
                 data-aos-delay="900"
                 data-aos-duration="1400"
                 className="gallery__item gallery__item--13"
               >
                 <img
-                  src="https://i.imgur.com/a880IHY.jpg"
+                  src={outsideAir}
                   className="gallery__img"
-                  alt="Outside Winter"
+                  alt="Outside Air"
                 />
               </figure>
 
-              <figure
+              {/* <figure
                 data-aos="fade-up-right"
                 data-aos-delay="600"
                 data-aos-duration="1500"
@@ -390,7 +382,7 @@ No pets over 25 lbs.
                   className="gallery__img"
                   alt="Outside Summer"
                 />
-              </figure>
+              </figure> */}
               
             </div>
           </div>
@@ -416,26 +408,41 @@ No pets over 25 lbs.
             <iframe data-aos="zoom-in"
             data-aos-delay="300"
             data-aos-duration="1500"  src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FGreenForestApts%2Fvideos%2F537745906753895%2F&show_text=false&width=560&t=2" width="560" height="314" style={{border:"none", overflow: "hidden"}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>,
-            "https://i.imgur.com/ILzD0HZ.jpg", // kitchen
-            "https://i.imgur.com/QSntCVw.jpg", // kitchen
-            "https://i.imgur.com/R16jAox.jpg", //bathroom
-            "https://i.imgur.com/QEGr0Yc.jpg", //bathroom
-            "https://i.imgur.com/MFPbkjh.jpg", //living vaulted
-            "https://i.imgur.com/9TLB0Wu.jpg", //living regular
-            "https://i.imgur.com/hzj7TXC.jpg", //living regular
-            "https://i.imgur.com/yafqqml.jpg", //living balcony door
-            "https://i.imgur.com/J4xLkW4.jpg", //bedroom
-            "https://i.imgur.com/t5fnmv0.jpg", //bedroom
-            "https://i.imgur.com/9Pc0exQ.jpg", //bedroom
-            "https://i.imgur.com/KmnAkPq.jpg", //bedroom
-            "https://i.imgur.com/YUamIMj.jpg", //living barshelf vaulted
-            "https://i.imgur.com/oYV2yrP.jpg", // kitchen window
-            "https://i.imgur.com/rUeTCeA.jpg", //entance
-            "https://i.imgur.com/jB7Yruf.jpg", //"halway"
-            "https://i.imgur.com/CwSBvsh.jpg", //floor plan
-            "https://i.imgur.com/U1Mtm2b.jpg", //outside drone
-            "https://i.imgur.com/a880IHY.jpg", //outside winter
-            "https://i.imgur.com/BCQ72z5.jpg", //outside summer
+            newKitchenImg,
+            newKitchenImg2,
+            oldCloset,
+            oldLiving,
+            oldLiving2,
+            newLiving2,
+            newLiving3,
+            oldBathroom,
+            newBedroom,
+            floorPlan,
+            oldBedroom2,
+            oldKitchen,
+            appliances,
+            outside,
+            outsideAir
+            // "https://i.imgur.com/ILzD0HZ.jpg", // kitchen
+            // "https://i.imgur.com/QSntCVw.jpg", // kitchen
+            // "https://i.imgur.com/R16jAox.jpg", //bathroom
+            // "https://i.imgur.com/QEGr0Yc.jpg", //bathroom
+            // "https://i.imgur.com/MFPbkjh.jpg", //living vaulted
+            // "https://i.imgur.com/9TLB0Wu.jpg", //living regular
+            // "https://i.imgur.com/hzj7TXC.jpg", //living regular
+            // "https://i.imgur.com/yafqqml.jpg", //living balcony door
+            // "https://i.imgur.com/J4xLkW4.jpg", //bedroom
+            // "https://i.imgur.com/t5fnmv0.jpg", //bedroom
+            // "https://i.imgur.com/9Pc0exQ.jpg", //bedroom
+            // "https://i.imgur.com/KmnAkPq.jpg", //bedroom
+            // "https://i.imgur.com/YUamIMj.jpg", //living barshelf vaulted
+            // "https://i.imgur.com/oYV2yrP.jpg", // kitchen window
+            // "https://i.imgur.com/rUeTCeA.jpg", //entance
+            // "https://i.imgur.com/jB7Yruf.jpg", //"halway"
+            // "https://i.imgur.com/CwSBvsh.jpg", //floor plan
+            // "https://i.imgur.com/U1Mtm2b.jpg", //outside drone
+            // "https://i.imgur.com/a880IHY.jpg", //outside winter
+            // "https://i.imgur.com/BCQ72z5.jpg", //outside summer
             
           ]}
         />
